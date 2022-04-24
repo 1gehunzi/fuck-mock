20ac612eec168137d9beac8ab395f87b713cc509
xiyuanyuan <xiyuanyan@meituan.com>
2022-04-24 12:13:37 +0800

    代码整理

--- src/background/index.js
+++ src/background/index.js
@@ -1,62 +1,76 @@
-import { AJAX_INTERCEPTOR_PROJECTS, AJAX_INTERCEPTOR_CURRENT_PROJECT } from '@/store'
+import {
+  AJAX_INTERCEPTOR_PROJECTS,
+  AJAX_INTERCEPTOR_CURRENT_PROJECT,
+} from '@/store'
 
-let ftdWindow = null;
+let ftdWindow = null
 
 chrome.windows.onRemoved.addListener((windowId) => {
-	if (ftdWindow && ftdWindow.id === windowId) {
-		console.log(`Close the window!`);
-		chrome.browserAction.setBadgeText({text: ""});
-		ftdWindow = null;
-	}
-});
+  if (ftdWindow && ftdWindow.id === windowId) {
+    console.log(`Close the window!`)
+    chrome.browserAction.setBadgeText({ text: '' })
+    ftdWindow = null
+  }
+})
 
 const defaultProject = {
   name: 'localhost',
   host: 'http://localhost:8080',
   color: '#409EFF',
-  switchOn: true
+  switchOn: true,
 }
 
-chrome.runtime.onInstalled.addListener(function() {
-  //   chrome.storage.local.set({[AJAX_INTERCEPTOR_PROJECTS]: [defaultProject], [AJAX_INTERCEPTOR_CURRENT_PROJECT]: defaultProject.name}, function() {
-  //   console.log('The color is green.');
-  // });
-});
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.local.set(
+    {
+      [AJAX_INTERCEPTOR_PROJECTS]: [defaultProject],
+      [AJAX_INTERCEPTOR_CURRENT_PROJECT]: defaultProject.name,
+    },
+    function () {
+      console.log('The color is green.')
+    }
+  )
+})
 
-chrome.browserAction.onClicked.addListener(function() {
-	console.log(`browserAction.onClicked`);
-	if (ftdWindow) {
-		console.log("The window exists!");
-		const info = {
-			focused: true
-		};
-		chrome.windows.update(ftdWindow.id, info, (w) => {
-			if (!w) {
-				console.log(`Error: The window does not exist!`);
-				ftdWindow = null;
-			}
-		});
-	} else {
-		chrome.storage.local.get(['windowSize'], function(result) {
-			console.log(`storage.sync`);
-			let width = 1000;
-			let height = 700;
-			if (result.windowSize) {
-				width = parseInt(result.windowSize.width);
-				height = parseInt(result.windowSize.height);
-			}
-		    const left = parseInt((window.screen.width - width) / 2);
-		    const top = parseInt((window.screen.height - height) / 2);
-		    console.log(`${left} ${top} ${width} ${height}`);
+chrome.browserAction.onClicked.addListener(function () {
+  console.log(`browserAction.onClicked`)
+  if (ftdWindow) {
+    console.log('The window exists!')
+    const info = {
+      focused: true,
+    }
+    chrome.windows.update(ftdWindow.id, info, (w) => {
+      if (!w) {
+        console.log(`Error: The window does not exist!`)
+        ftdWindow = null
+      }
+    })
+  } else {
+    chrome.storage.local.get(['windowSize'], function (result) {
+      console.log(`storage.sync`)
+      let width = 1000
+      let height = 700
+      if (result.windowSize) {
+        width = parseInt(result.windowSize.width)
+        height = parseInt(result.windowSize.height)
+      }
+      const left = parseInt((window.screen.width - width) / 2)
+      const top = parseInt((window.screen.height - height) / 2)
+      console.log(`${left} ${top} ${width} ${height}`)
 
-		    chrome.windows.create({
-		      url: chrome.runtime.getURL("webContent.html"),
-		      type: "popup",
-		      left, top, width, height
-		    }, function(window) {
-		    	ftdWindow = window;
-		    });
-		});
-	}
-});
-
+      chrome.windows.create(
+        {
+          url: chrome.runtime.getURL('webContent.html'),
+          type: 'popup',
+          left,
+          top,
+          width,
+          height,
+        },
+        function (window) {
+          ftdWindow = window
+        }
+      )
+    })
+  }
+})
--- src/contentScripts/index.js
+++ src/contentScripts/index.js
@@ -1,4 +1,7 @@
-import { AJAX_INTERCEPTOR_PROJECTS, AJAX_INTERCEPTOR_CURRENT_PROJECT } from '@/store'
+import {
+  AJAX_INTERCEPTOR_PROJECTS,
+  AJAX_INTERCEPTOR_CURRENT_PROJECT,
+} from '@/store'
 
 const keys = [AJAX_INTERCEPTOR_PROJECTS, AJAX_INTERCEPTOR_CURRENT_PROJECT]
 
@@ -13,47 +16,50 @@ function injectScriptToPage() {
   input.setAttribute('style', 'display:none')
   document.documentElement.appendChild(input)
 }
-// TODO: 搞下什么界面被mock
-chrome.storage.local.get(keys, (result) => {
-  const currentName = result[AJAX_INTERCEPTOR_CURRENT_PROJECT]
-  const projectList = result[AJAX_INTERCEPTOR_PROJECTS] || []
-  // eslint-disable-next-line no-restricted-globals
-  const {origin} = location
-  const currentProject = projectList.find(item => item.name === currentName) || {}
-  // 注意host和origin是否带结尾的 slush
-  if (origin === currentProject.host) {
-    injectScriptToPage()
-  }
-})
-
-window.addEventListener(
-  'CUSTOMEVENT',
-  function (event) {
-      chrome.runtime.sendMessage(chrome.runtime.id, {type: 'ajaxInterceptor', to: 'background', detail: event.detail});
-  },
-  false
-)
-
 const executeScript = (data) => {
   const code = JSON.stringify(data)
   if (document.getElementById('ajaxInterceptor')) {
     document.getElementById('ajaxInterceptor').value = code
   }
 }
-const injectGlobalData = () => {
+// 让 storage 的数据实时反映到页面上
+const setGlobalData = () => {
   chrome.storage.local.get(keys, (result) => {
     executeScript(result)
   })
 }
-// TODO: 设置 icon 是否生效
-injectGlobalData()
+chrome.storage.local.get(keys, (result) => {
+  const currentName = result[AJAX_INTERCEPTOR_CURRENT_PROJECT]
+  const projectList = result[AJAX_INTERCEPTOR_PROJECTS] || []
+  // eslint-disable-next-line no-restricted-globals
+  const { origin } = location
+  const currentProject =
+    projectList.find((item) => item.name === currentName) || {}
+  // 注意host和origin是否带结尾的 slush
+  if (origin === currentProject.host) {
+    injectScriptToPage()
+    setGlobalData()
+  }
+})
+
+window.addEventListener(
+  'CUSTOMEVENT',
+  (event) => {
+    chrome.runtime.sendMessage(chrome.runtime.id, {
+      type: 'ajaxInterceptor',
+      to: 'background',
+      detail: event.detail,
+    })
+  },
+  false
+)
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
   // eslint-disable-next-line no-restricted-syntax
   for (const [key, { oldValue, newValue }] of Object.entries(changes)) {
     // console.log(namespace, oldValue, newValue)
     if (keys.find((item) => item === key)) {
-      injectGlobalData()
+      setGlobalData()
     }
   }
 })
